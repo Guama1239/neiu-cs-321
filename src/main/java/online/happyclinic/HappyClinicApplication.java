c122d92ef5405115d1aabce6d2bc814460f0e02f
guama1239 <guama1239@gmail.com>
2020-09-10 16:44:41 -0500

    hw update files

---
+++ src/main/java/online/happyclinic/HappyClinicApplication.java
@@ -0,0 +1,13 @@
+package online.happyclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/online/happyclinic/HappyClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package online.happyclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
